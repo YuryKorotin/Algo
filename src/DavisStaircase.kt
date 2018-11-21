a50234a0df6d094fa53368f4590fea2a732a86f2
YuryKorotin <yurykorotin73@gmail.com>
2018-11-21 12:55:00 +0400

    Add cache for storing results

--- src/DavisStaircase.kt
+++ src/DavisStaircase.kt
@@ -1,8 +1,23 @@
 package solution
 
+import javax.swing.UIManager.put
+import java.util.HashMap
+
+
+
+
+
 open class DavisStaircase {
 
-    fun stepPerms(n: Int): Int {
+    fun stepPerms(n: Int, cache: HashMap<Int, Int>): Int {
+        if (n < 0) {
+            return 0
+        }
+
+        if (n == 0) {
+            return 1
+        }
+
         if (n < 3) {
             return n
         }
@@ -11,7 +26,15 @@ open class DavisStaircase {
             return 4
         }
 
-        return stepPerms(n - 1) + stepPerms(n - 2) + stepPerms(n - 3)
+        if (!cache.containsKey(n)) {
+            val count = stepPerms(n - 1, cache) +
+                    stepPerms(n - 2, cache) +
+                    stepPerms(n - 3, cache)
+
+            cache.put(n, count)
+        }
+
+        return cache.get(n)!!
     }
 
     open fun calculate(input : List<String>) : List<String> {
@@ -19,8 +42,11 @@ open class DavisStaircase {
 
         val queue = QueueOnList<Int>()
 
+
+        val cache = HashMap<Int, Int>()
+
         input.forEach { item ->
-            result.add(stepPerms(item.toInt()).toString())
+            result.add(stepPerms(item.toInt(), cache).toString())
         }
 
         return result
