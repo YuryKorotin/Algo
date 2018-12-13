1dff983e0e40b41f3899a497c32ba541f4822a77
YuryKorotin <yurykorotin73@gmail.com>
2018-12-13 12:39:41 +0400

    Optimizing by using binary search

--- src/FraudulentActivity.kt
+++ src/FraudulentActivity.kt
@@ -2,54 +2,76 @@ package solution
 
 open class FraudulentActivity {
     fun activityNotifications(expenditure: Array<Int>, d: Int): Int {
-        /*val endOfRange = expenditure.size
-        var numberOfNotifications = 0
-
-        var sliced = expenditure.slice(0..d - 1).toMutableList()
-        var sum = sliced.sum().toDouble()
-        var mediana : Double = sum / d
-
-        for(i in d until endOfRange) {
-            if (expenditure[i] >= mediana * 2) {
-                numberOfNotifications += 1
-            }
-
-            sliced.removeAt(0)
-            val newItem = expenditure[i]
-            sliced.add(newItem)
-
-            sum = sliced.sum().toDouble()
-            mediana = sum / d
-        }
-
-        return numberOfNotifications*/
         val endOfRange = expenditure.size
         var numberOfNotifications = 0
 
         var sliced = expenditure.slice(0..d - 1).sorted().toMutableList()
 
-        for(i in d until endOfRange) {
-            val mediana = sliced.get(d / 2)
+        var mediana = 0
 
-            if (expenditure[i] >= mediana * 2) {
+        for(i in d until endOfRange) {
+            if (d % 2 != 0) {
+                mediana = sliced.get(d / 2) * 2
+            } else {
+                mediana = sliced.get(d / 2) + sliced.get(d / 2 - 1)
+            }
+
+            if (expenditure[i] >= mediana) {
                 numberOfNotifications += 1
             }
 
             sliced.remove(expenditure[i - d])
 
             val newItem = expenditure[i]
+            val sliceSize =  sliced.size
 
-            for (j in 0 until sliced.size) {
-                if (newItem <= sliced.get(j)) {
-                    sliced.add(j, newItem)
-                    break
-                }
-                if (j == sliced.size - 1) {
-                    sliced.add(newItem)
-                }
+            if (newItem > sliced.get(sliceSize - 1)) {
+                sliced.add(newItem)
+                continue
             }
+
+            sliced.add(
+                    findBestIndex(
+                            newItem,
+                            sliced,
+                            0,
+                            sliceSize - 1),
+                    newItem)
         }
 
         return numberOfNotifications
+    }
+
+    fun findBestIndex(item: Int,
+                      list: MutableList<Int>,
+                      startPosition: Int,
+                      endPosition: Int) : Int{
+        val rangeSize = (endPosition - startPosition) / 2
+
+        if (item == list.get(startPosition)) {
+            return startPosition
+        }
+
+        if (item == list.get(endPosition)) {
+            return endPosition
+        }
+
+        if (rangeSize == 0) {
+            return endPosition
+        }
+
+        if (item > list.get(startPosition + rangeSize)) {
+            return findBestIndex(
+                    item,
+                    list,
+                    startPosition + rangeSize,
+                    endPosition)
+        } else {
+            return findBestIndex(
+                    item,
+                    list,
+                    startPosition,
+                    startPosition + rangeSize)
+        }
     }
 }
