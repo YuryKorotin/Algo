5034f4d0c1a4e8cceac56169d947ac31348f7409
YuryKorotin <yurykorotin73@gmail.com>
2019-02-04 22:39:28 +0400

    Add script file for solutions from book

---
+++ src/cracking_coding_interview_problems/solution_launcher.kts
@@ -0,0 +1,16 @@
+class SolutionSolver {
+    companion object {
+
+        fun checkSolution(fileName: String, problemName: String) {
+            println("Start checking of problem solution$problemName ...")
+
+            println("End checking of problem solution $problemName")
+        }
+
+        fun solveProblem() {
+            println("Start solution of problem $problemName...")
+
+            println("End solution of problem $problemName")
+        }
+    }
+}
