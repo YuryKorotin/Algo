396e90d641df1a0c4bf1ba85814e6e1df372307d
YuryKorotin <yurykorotin73@gmail.com>
2019-02-14 22:03:21 +0400

    Add brute force solution

---
+++ src/cracking_coding_interview_problems/ZeroMatrix.kt
@@ -0,0 +1,38 @@
+//INCLUDE Solution.kt
+
+class ZeroMatrix: Solution {
+    fun execute(source: Array<Array<Int>>): Array<Array<Int>>{
+        var size = source.size
+
+        val result = Array(size, {
+            Array(size, {
+                0
+            })
+        })
+
+        val indicesForEmptyRows = MutableList<Int>(size, {0})
+        val indicesForEmptyColumns = MutableList<Int>(size, {0})
+
+        for (i in 0 until size) {
+            for (j in 0 until size) {
+                if (source[i][j] == 0) {
+                    indicesForEmptyColumns.add(j)
+                    indicesForEmptyRows.add(i)
+                }
+            }
+        }
+
+        for (i in 0 until size) {
+            for (j in 0 until size) {
+                if (indicesForEmptyColumns.contains(j) ||
+                        indicesForEmptyRows.contains(i)) {
+                    result[i][j] = 0
+                } else {
+                    result[i][j] = source[i][j]
+                }
+            }
+        }
+
+        return result
+    }
+}
