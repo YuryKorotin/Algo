241015b6aba32dd946350d0da2a537d09da8dcef
YuryKorotin <yurykorotin73@gmail.com>
2019-02-19 23:50:36 +0400

    Complete solution

--- src/cracking_coding_interview_problems/StringRotation.kt
+++ src/cracking_coding_interview_problems/StringRotation.kt
@@ -1,7 +1,24 @@
 //INCLUDE Solution.kt
 
 class StringRotation: Solution {
-    fun isSubtring(firstSouce: String, secondSource: String): Boolean {
-        return false
+    fun isSubtring(firstSource: String, secondSource: String): Boolean {
+        var hasCommonSequence = false
+
+        var firstDoubled = firstSource + firstSource
+
+        loop@for (i in 0 until firstDoubled.length) {
+            for (j in 0 until secondSource.length) {
+                if (firstDoubled[i] == secondSource[j]) {
+                    hasCommonSequence = true
+                }
+
+                if (firstSource[i] != secondSource[j] && hasCommonSequence) {
+                    hasCommonSequence
+                    break@loop
+                }
+            }
+        }
+
+        return hasCommonSequence
     }
 }
