6542027730e890891cca5bd55b19cd33907155ab
YuryKorotin <yurykorotin73@gmail.com>
2019-03-01 16:10:58 +0400

    Add deleting middle

--- src/cracking_coding_interview_problems/DeleteMiddleNode.kt
+++ src/cracking_coding_interview_problems/DeleteMiddleNode.kt
@@ -2,6 +2,28 @@
 //INCLUDE LinkedListNode.kt
 
 class DeleteMiddleNode : Solution {
+    var sizeCounter = 0
+    var k = 0
+
     fun execute(head: LinkedListNode?) {
+        exploreNode(head)
+    }
+
+    fun exploreNode(node: LinkedListNode?): LinkedListNode? {
+        if (node == null) {
+            return node
+        }
+
+        sizeCounter++
+
+        exploreNode(node!!.next)
+
+        if (k == sizeCounter / 2 + 1) {
+            node!!.next = node!!.next!!.next
+        }
+
+        k++
+
+        return node
     }
 }
--- src/cracking_coding_interview_problems/solution_launcher.kts
+++ src/cracking_coding_interview_problems/solution_launcher.kts
@@ -172,10 +172,10 @@ val deleteMiddleNodeTest = { solution: Solution ->
 
     val remover = solution as DeleteMiddleNode
 
-    val sourceList = listOf(5, 4, 3, 2, 1)
-    val resultList = listOf(4, 3, 1)
+    val sourceList = listOf(7, 12, 1, 2, 3, 4, 5)
+    val resultList = listOf(5, 4, 3, 1, 12, 7)
 
-    var current: LinkedListNode? = LinkedListNode(null, 3)
+    var current: LinkedListNode? = LinkedListNode(null, sourceList[0])
     var newNode: LinkedListNode? = null
 
     for (i in 1 until sourceList.size) {
@@ -185,7 +185,13 @@ val deleteMiddleNodeTest = { solution: Solution ->
 
     remover.execute(current)
 
-    assertTrue(false)
+    var node = current
+
+    resultList.forEach {
+        assertTrue(it == node!!.data)
+
+        node = node!!.next
+    }
 }
 
 var checker = PalindromePermutationChecker()
