feeb9c62c2666f01386005d873c615e0fb88ad79
YuryKorotin <yurykorotin73@gmail.com>
2019-03-07 00:23:08 +0400

    Fix error in changing of fast

--- src/cracking_coding_interview_problems/PalindromeChecker.kt
+++ src/cracking_coding_interview_problems/PalindromeChecker.kt
@@ -9,8 +9,32 @@ class PalindromeChecker : Solution {
         var stack = StackWithList()
 
         while(fast != null && fast!!.next != null) {
-            stack.push(slow.data)
+            stack.push(slow!!.data)
+
+            val data = slow!!.data
+
+            slow = slow!!.next
+
+            fast = fast!!.next!!.next
         }
+
+        if (fast != null) {
+            slow = slow!!.next
+        }
+
+        while (slow != null) {
+            val top = stack.pop()
+
+            println(top)
+
+            println(slow!!.data)
+
+            if (top != slow!!.data) {
+                return false
+            }
+            slow = slow!!.next
+        }
+        return true
     }
 }
 
