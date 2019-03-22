357c9c1a5aeaccbad2d4b178f83526d7074172f9
YuryKorotin <yurykorotin73@gmail.com>
2019-03-22 23:10:31 +0400

    Add push implementation

--- src/cracking_coding_interview_problems/solutions/StackMin.kt
+++ src/cracking_coding_interview_problems/solutions/StackMin.kt
@@ -2,7 +2,8 @@
 
 class StackMin: Solution, Stack<NodeWithMin> {
     fun push(element: Int) {
-        int newMin = kotlin.math.min(element, min())
+        var newMin = kotlin.math.min(element, min())
+        super.push(NodeWithMin(element, newMin))
     }
 
     fun pop(): Int {
@@ -13,5 +14,23 @@ class StackMin: Solution, Stack<NodeWithMin> {
         return 0
     }
 }
+
+class Stack<T>{
+    val elements: MutableList<T> = mutableListOf()
+    fun isEmpty() = elements.isEmpty()
+    fun count() = elements.size
+    fun push(item: T) = elements.add(item)
+    fun pop() : T? {
+        val item = elements.lastOrNull()
+        if (!isEmpty()){
+            elements.removeAt(elements.size -1)
+        }
+        return item
+    }
+    fun peek() : T? = elements.lastOrNull()
+    override fun toString(): String = elements.toString()
+}
+
+fun <T> Stack<T>.push(items: Collection<T>) = items.forEach { this.push(it) }
 
 data class NodeWithMin(var value: Int, var min: Int)
