585dc6efe63e7ef94c01f8201d58f4f2fccdefd9
YuryKorotin <yurykorotin73@gmail.com>
2019-04-23 00:39:48 +0400

    Add tests

--- src/cracking_coding_interview_problems/solutions/AnimalShelter.kt
+++ src/cracking_coding_interview_problems/solutions/AnimalShelter.kt
@@ -8,5 +8,55 @@ open class Animal(var name: String = "") {
 
 class Dog(name: String = ""): Animal(name)
 
+class Cat(name: String = ""): Animal(name)
+
+
 class AnimalShelter: Solution {
+    val dogs: MutableList<Dog> = mutableListOf()
+    val cats: MutableList<Cat> = mutableListOf()
+    var order: Int = 0
+
+    fun enqueue(animal: Animal) {
+        animal.order = order
+        order++
+        if (animal is Dog) {
+            dogs.add(animal)
+        } else if (animal is Cat) {
+            cats.add(animal)
+        }
+    }
+
+    fun dequeueAny(): Animal {
+        if (dogs.isEmpty()) {
+            return dequeueCats()
+        } else if (cats.isEmpty()) {
+            return dequeueDogs()
+        }
+
+        val dog = dogs.get(dogs.size - 1)
+
+        val cat = cats.get(cats.size - 1)
+
+        if (dog.isOlderThan(cat)) {
+            return dequeueDogs()
+        } else {
+            return dequeueCats()
+        }
+    }
+
+    fun dequeueCats(): Cat {
+        val cat = cats.get(cats.size - 1)
+
+        cats.remove(cat)
+
+        return cat
+    }
+
+    fun dequeueDogs(): Dog {
+        val dog = dogs.get(dogs.size - 1)
+
+        dogs.remove(dog)
+
+        return dog
+    }
 }
--- src/cracking_coding_interview_problems/tests/AnimalShelterTest.kt
+++ src/cracking_coding_interview_problems/tests/AnimalShelterTest.kt
@@ -1,4 +1,14 @@
 val animalShelterTest = { solution: Solution ->
     val animalShelter = solution as AnimalShelter
 
+    val testCat = Cat("Kitty")
+    val testDog = Dog("Bobby")
+
+    animalShelter.enqueue(testCat)
+
+    animalShelter.enqueue(testDog)
+
+    assertTrue(animalShelter.dequeueCats() === testCat)
+
+    assertTrue(animalShelter.dequeueAny() === testDog)
 }
