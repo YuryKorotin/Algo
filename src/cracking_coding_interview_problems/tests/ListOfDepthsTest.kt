72203955b250e8c3017933d0d2b734808123362f
YuryKorotin <yurykorotin73@gmail.com>
2019-05-14 11:36:47 +0400

    Add test with childs

--- src/cracking_coding_interview_problems/tests/ListOfDepthsTest.kt
+++ src/cracking_coding_interview_problems/tests/ListOfDepthsTest.kt
@@ -1,12 +1,16 @@
 val listOfDepthsTest = { solution: Solution ->
+
+    var leftChild = TreeNode(2, null, null)
+
+    var rightChild = TreeNode(5, null, null)
    
-    val root = TreeNode(3, null, null)
+    val root = TreeNode(3, leftChild, rightChild)
     
     val listOfDepths = solution as ListOfDepths
 
     listOfDepths.root = root
     listOfDepths.fill()
    
-    assertTrue(listOfDepths.getSize() == 1)
+    assertTrue(listOfDepths.getSize() == 2)
 }
 
