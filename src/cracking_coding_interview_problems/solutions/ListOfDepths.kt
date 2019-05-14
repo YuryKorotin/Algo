5190f67dfb3e253d433b712814fdb8bf9b609954
YuryKorotin <yurykorotin73@gmail.com>
2019-05-14 11:19:46 +0400

    Add solution for levels filling

--- src/cracking_coding_interview_problems/solutions/ListOfDepths.kt
+++ src/cracking_coding_interview_problems/solutions/ListOfDepths.kt
@@ -1,10 +1,38 @@
 class ListOfDepths(): Solution {
    var root: TreeNode? = null
-   var listOfLevels: MutableList<List<TreeNode>> = mutableListOf<List<TreeNode>>()
+   var listOfLevels: MutableList<MutableList<TreeNode>> = mutableListOf<MutableList<TreeNode>>()
    fun getSize(): Int {
      return listOfLevels.size
    }
-   fun fill() {
 
+   fun fillLevels(node: TreeNode?, level: Int) {
+      if (node == null) {
+         return
+      }
+
+      var levelList: MutableList<TreeNode> = mutableListOf()
+
+      if (listOfLevels.size == level) {
+         listOfLevels.add(levelList)
+      } else {
+         levelList = listOfLevels[level]
+      }
+
+      levelList.add(node)
+      if (node!!.left != null) {
+         fillLevels(node!!.left, level + 1)
+      }
+
+      if (node!!.right != null) {
+         fillLevels(node!!.right, level + 1)
+      }
+   }
+
+   fun fill() {
+      if (root == null) {
+         return
+      }
+
+      fillLevels(root, 0)
    }
 }
--- src/cracking_coding_interview_problems/tests/ListOfDepthsTest.kt
+++ src/cracking_coding_interview_problems/tests/ListOfDepthsTest.kt
@@ -3,8 +3,10 @@ val listOfDepthsTest = { solution: Solution ->
     val root = TreeNode(3, null, null)
     
     val listOfDepths = solution as ListOfDepths
-   
-    assertTrue(listOfDepths.getSize() == 0)
 
+    listOfDepths.root = root
+    listOfDepths.fill()
+   
+    assertTrue(listOfDepths.getSize() == 1)
 }
 
