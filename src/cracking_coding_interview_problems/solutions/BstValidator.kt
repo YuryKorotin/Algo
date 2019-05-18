5772189b9e04dc1ee4c19889df635b0f3882816b
YuryKorotin <yurykorotin73@gmail.com>
2019-05-18 09:09:33 +0400

    Add min and max solution

--- src/cracking_coding_interview_problems/solutions/BstValidator.kt
+++ src/cracking_coding_interview_problems/solutions/BstValidator.kt
@@ -2,6 +2,22 @@ class BstValidator: Solution {
     var root: TreeNode? = null
 
     fun isTreeValid(): Boolean {
-        return false
+        return checkBst(root, null, null)
+    }
+
+    fun checkBst(node: TreeNode?, min: Int?, max: Int?): Boolean {
+        if (node == null) {
+            return true
+        }
+
+        if ((min != null && node!!.data <= min) || (max != null && node!!.data > max)) {
+            return false
+        }
+
+        if (!checkBst(node!!.left, min, node!!.data) || !checkBst(node!!.right, node!!.data, max)) {
+            return false
+        }
+
+        return true
     }
 }
