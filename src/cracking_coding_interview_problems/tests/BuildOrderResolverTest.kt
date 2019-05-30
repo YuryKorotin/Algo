c2d4df4516cb2d7dbdbdabbb6b2f288728256daa
YuryKorotin <yurykorotin73@gmail.com>
2019-05-30 12:43:05 +0400

    Add simple build order test

--- src/cracking_coding_interview_problems/tests/BuildOrderResolverTest.kt
+++ src/cracking_coding_interview_problems/tests/BuildOrderResolverTest.kt
@@ -2,5 +2,16 @@ val buildOrderResolverTest = { solution: Solution ->
 
     val buildOrderResolver = solution as BuildOrderResolver
 
-    assertTrue(false)
+    val projects = listOf("a", "b", "c", "d", "e", "f")
+
+    val dependencies = listOf(
+            listOf("a", "d"),
+            listOf("f", "b"),
+            listOf("b", "d"),
+            listOf("f", "a"),
+            listOf("d", "c"))
+
+    val order = buildOrderResolver.findBuildOrder(projects, dependencies)
+
+    assertTrue("f" == order.peek()!!.name)
 }
