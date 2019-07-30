f62fb0cf9520536caa98f627aa3cee2f20aa04fa
YuryKorotin <dreadnoughtfull@yandex.ru>
2019-07-30 18:46:25 +0400

    Add test

--- src/cracking_coding_interview_problems/solutions/BinarySearchTreeSequenceTracer.kt
+++ src/cracking_coding_interview_problems/solutions/BinarySearchTreeSequenceTracer.kt
@@ -1,5 +1,30 @@
 class BinarySearchTreeSequenceTracer: Solution {
-	fun traceTree(head: TreeNode): List<String> {
-		return listOf("")
+
+	fun traceTree(node: TreeNode?): MutableList<MutableList<Int>> {
+		var result: MutableList<MutableList<Int> = mutableListOf()
+		if (node == null) {
+			result.add(mutableListOf<Int>())
+			return result
+		}
+		
+		val trace: String = mutableListOf(head!!.data)
+		var currentNode: TreeNode? = node	
+
+		trace.add(node.data.toString())
+		var temp = 
+		
+		if(currentNode!!.left != null) {
+			trace.add(traceTree(currentNode!!.left))
+		}
+
+		if(currentNode!!.right != null) {
+			trace.add(traceTree(currentNode!!.right))
+		}
+
+		return trace
+	}
+
+	fun weaveList(first: MutableList<Int>, second: MutableList<Int>, results: MutableList<<>>) {
+
 	}
 }
--- src/cracking_coding_interview_problems/tests/BinarySearchTreeSequenceTracerTest.kt
+++ src/cracking_coding_interview_problems/tests/BinarySearchTreeSequenceTracerTest.kt
@@ -1,3 +1,19 @@
 val binarySearchTreeSequenceTracerTest = { solution: Solution ->
-	assertTrue(true)
+	val testResult = listOf(listOf(2, 1, 3), listOf(2, 3, 1))
+
+	val tracer = solution as BinarySearchTreeSequenceTracer
+
+	var leftChild = TreeNode(1, null, null)
+
+    var rightChild = TreeNode(3, null, null)
+
+    val root = TreeNode(2, leftChild, rightChild)
+
+    leftChild.parent = root
+
+    rightChild.parent = root
+
+	val result = tracer.traceTree(root)
+
+	assertTrue(testResult == result)
 }
